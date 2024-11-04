ebaaa7c22c8f55a90e40d25afe45995d2276f24e
yogesh0071 <yogesh.nimje01@outlook.com>
2024-11-04 02:37:04 +0000

    nf

---
+++ python.py
@@ -0,0 +1,13 @@
+##hello world
+
+#print ("hello world")
+
+## hello world with a variable
+#msg = "hello world"
+#print(msg)
+
+#f-string using varisable in string
+#first_name = 'yogesh'
+#last_name = 'nimje'
+#full_name = f"{first_name} {last_name}"
+#print(full_name) 
